e40a572d46a951c15f0b59493cf9faf16b8cf1dd
Saiglor <jakimets.2012@gmail.com>
2021-12-05 16:57:58 +0500

    Добавил контекст

---
+++ Otus.PostgreSql/ApplicationContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Otus.Models.Models;
+using Otus.PostgreSql.Configurations;
+
+namespace Otus.PostgreSql
+{
+    public class ApplicationContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public DbSet<ClientProfileModel> ClientProfiles { get; set; }
+        public DbSet<ItemModel> Items { get; set; }
+        public DbSet<ItemTypeModel> ItemTypes { get; set; }
+
+        public ApplicationContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new ClientProfileConfiguration());
+            modelBuilder.ApplyConfiguration(new ItemConfiguration());
+            modelBuilder.ApplyConfiguration(new ItemTypeConfiguration());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_connectionString).UseLazyLoadingProxies();
+        }
+    }
+}
